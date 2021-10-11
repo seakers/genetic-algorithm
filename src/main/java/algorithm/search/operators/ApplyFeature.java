3204efc7d781a1a94266e49401f6234bcd60d2cc
Antoni Viros <ani300@gmail.com>
2021-10-11 17:07:40 -0500

    Started adding feature types

--- src/main/java/algorithm/search/operators/ApplyFeature.java
+++ src/main/java/algorithm/search/operators/ApplyFeature.java
@@ -5,6 +5,10 @@ import org.moeaframework.core.variable.BinaryVariable;
 
 import algorithm.search.problems.Assigning.AssigningArchitecture;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.moeaframework.core.PRNG;
 import org.moeaframework.core.Solution;
 import org.moeaframework.core.Variable;
 
@@ -13,8 +17,10 @@ public class ApplyFeature implements Variation {
     private final int numInstruments;
     private final int numOrbits;
 
-    public ApplyFeature(String feature) {
+    public ApplyFeature(String feature, int numInstruments, int numOrbits) {
         this.feature = feature;
+        this.numInstruments = numInstruments;
+        this.numOrbits = numOrbits;
     }
 
     /**
@@ -46,50 +52,121 @@ public class ApplyFeature implements Variation {
         }
         Solution child = parents[0].copy();
         if (child instanceof AssigningArchitecture) {
+            // First, preprocess feature to get all the information {baseFeature[orbits;instruments;numbers]}
+            Pattern featurePattern = Pattern.compile("{(\\w+)\\[(\\d+(?:,\\d+)*)?;(\\d+(?:,\\d+)*)?;(\\d+(?:,\\d+)*)?\\]}");
+            Matcher featureInfo = featurePattern.matcher(this.feature);
+            String featureType = featureInfo.group(1);
             // Variables start at 1 because AssigningArchitecture has a combinatorial variable first
-            for (int i = 1; i < child.getNumberOfVariables(); i++) {
-                Variable var = child.getVariable(i);
-                if (var instanceof BinaryVariable) {
-                    BinaryVariable binVar = (BinaryVariable)var;
-                    //check if the number of satellites is within the allowable bounds
-                    while (constelVar.getNumberOfSatellites() < constelVar.getSatelliteBound().getLowerBound()) {
-                        //turn on a random satellite that is off
-                        ArrayList<BooleanSatelliteVariable> offSats = new ArrayList<>();
-                        for (SatelliteVariable sat : constelVar.getSatelliteVariables()) {
-                            if (!((BooleanSatelliteVariable) sat).getManifest()) {
-                                offSats.add((BooleanSatelliteVariable) sat);
-                            }
-                        }
-                        int select = PRNG.nextInt(offSats.size());
-                        offSats.get(select).setManifest(true);
-                    }
-                    while (constelVar.getNumberOfSatellites() > constelVar.getSatelliteBound().getUpperBound()) {
-                        //turn off a random satellite that is on
-                        ArrayList<BooleanSatelliteVariable> onSats = new ArrayList<>();
-                        for (SatelliteVariable sat : constelVar.getSatelliteVariables()) {
-                            if (((BooleanSatelliteVariable) sat).getManifest()) {
-                                onSats.add((BooleanSatelliteVariable) sat);
-                            }
-                        }
-                        int select = PRNG.nextInt(onSats.size());
-                        onSats.get(select).setManifest(false);
-                    }
-                } else if(var instanceof ConstellationVariable){
-                    ConstellationVariable constelVar = (ConstellationVariable)var;
-                    //check if the number of satellites is within the allowable bounds
-                    if (constelVar.getNumberOfSatellites() < constelVar.getSatelliteBound().getLowerBound()) {
-    
-                    } 
-                    while(constelVar.getNumberOfSatellites() > constelVar.getSatelliteBound().getUpperBound()) {
-                        //remove a random sallite from the chromosome
-                        ArrayList<SatelliteVariable> sats = new ArrayList<>(constelVar.getSatelliteVariables());
-                        sats.remove(PRNG.nextInt(sats.size()));
-                        constelVar.setSatelliteVariables(sats);
-                    }
-                }
+            if (featureType == "present") {
+                String[] instruments = featureInfo.group(3).split(",", 0);
+                this.applyPresentFeature((AssigningArchitecture)child, instruments);
+            }
+            else if (featureType == "absent") {
+                String[] instruments = featureInfo.group(3).split(",", 0);
+                this.applyAbsentFeature((AssigningArchitecture)child, instruments);
+            }
+            else if (featureType == "numOfInstruments") {
+                String instruments = featureInfo.group(3);
+                Integer number = Integer.parseInt(featureInfo.group(4));
+                this.applyNumInstrumentsFeature((AssigningArchitecture)child, instruments, number);
             }
         }
         return new Solution[]{child};
+    }
+
+    public void applyPresentFeature(AssigningArchitecture solution, String[] instruments) {
+        // First, check if solution already has feature
+        boolean hasFeature = false;
+        Integer instrument = Integer.parseInt(instruments[0]);
+        for (int i = 1 + instrument; i < solution.getNumberOfVariables(); i += this.numInstruments) {
+            if (((BinaryVariable)solution.getVariable(i)).get(0)) {
+                hasFeature = true;
+            }
+        }
+        // Then, if solution doesn't have feature, apply it randomly
+        if (!hasFeature) {
+            int randomOrbit = PRNG.nextInt(0, this.numOrbits);
+            ((BinaryVariable)solution.getVariable(randomOrbit*this.numInstruments + instrument + 1)).set(0, true);
+        }
+    }
+
+    public void applyAbsentFeature(AssigningArchitecture solution, String[] instruments) {
+        // First, check if solution already has feature
+        boolean hasFeature = false;
+        Integer instrument = Integer.parseInt(instruments[0]);
+        for (int i = 1 + instrument; i < solution.getNumberOfVariables(); i += this.numInstruments) {
+            if (!((BinaryVariable)solution.getVariable(i)).get(0)) {
+                hasFeature = true;
+            }
+        }
+        // Then, if solution doesn't have feature, apply it randomly
+        if (!hasFeature) {
+            int randomOrbit = PRNG.nextInt(0, this.numOrbits);
+            ((BinaryVariable)solution.getVariable(randomOrbit*this.numInstruments + instrument + 1)).set(0, false);
+        }
+    }
+
+    public void applyNumInstrumentsFeature(AssigningArchitecture solution, String instruments, Integer number) {
+        if (instruments.length() == 0) {
+            //numOfInstruments[;;n]: Number of instruments in total
+            boolean hasFeature = false;
+            int count = 0;
+            for (int i = 1; i < solution.getNumberOfVariables(); i += 1) {
+                if (!((BinaryVariable)solution.getVariable(i)).get(0)) {
+                    hasFeature = true;
+                }
+            }
+            // Then, if solution doesn't have feature, apply it randomly
+            if (!hasFeature) {
+                int randomOrbit = PRNG.nextInt(0, this.numOrbits);
+                ((BinaryVariable)solution.getVariable(randomOrbit*this.numInstruments + instrument + 1)).set(0, false);
+            }
+        }
+        
+
+        //numOfInstruments[;i;n]: Number of instrument i
+        String[] instruments = featureInfo.group(3).split(",", 0);
+        boolean hasFeature = false;
+        Integer instrument = Integer.parseInt(instruments[0]);
+        for (int i = 1 + instrument; i < solution.getNumberOfVariables(); i += this.numInstruments) {
+            if (!((BinaryVariable)solution.getVariable(i)).get(0)) {
+                hasFeature = true;
+            }
+        }
+        // Then, if solution doesn't have feature, apply it randomly
+        if (!hasFeature) {
+            int randomOrbit = PRNG.nextInt(0, this.numOrbits);
+            ((BinaryVariable)solution.getVariable(randomOrbit*this.numInstruments + instrument + 1)).set(0, false);
+        }
+        
+        //numOfInstruments[;i,j,k;n]: Number of instruments in set {i, j, k} across all orbits
+        String[] instruments = featureInfo.group(3).split(",", 0);
+        boolean hasFeature = false;
+        Integer instrument = Integer.parseInt(instruments[0]);
+        for (int i = 1 + instrument; i < solution.getNumberOfVariables(); i += this.numInstruments) {
+            if (!((BinaryVariable)solution.getVariable(i)).get(0)) {
+                hasFeature = true;
+            }
+        }
+        // Then, if solution doesn't have feature, apply it randomly
+        if (!hasFeature) {
+            int randomOrbit = PRNG.nextInt(0, this.numOrbits);
+            ((BinaryVariable)solution.getVariable(randomOrbit*this.numInstruments + instrument + 1)).set(0, false);
+        }
+
+        // First, check if solution already has feature
+        boolean hasFeature = false;
+        Integer instrument = Integer.parseInt(instruments[0]);
+        for (int i = 1 + instrument; i < solution.getNumberOfVariables(); i += this.numInstruments) {
+            if (!((BinaryVariable)solution.getVariable(i)).get(0)) {
+                hasFeature = true;
+            }
+        }
+        // Then, if solution doesn't have feature, apply it randomly
+        if (!hasFeature) {
+            int randomOrbit = PRNG.nextInt(0, this.numOrbits);
+            ((BinaryVariable)solution.getVariable(randomOrbit*this.numInstruments + instrument + 1)).set(0, false);
+        }
     }
     
 }
